ef447d33b72efd867b21cc24be4b039fec35e1b8
Niels Korschinsky <niels.korschinsky@mni.thm.de>
2021-04-22 15:21:24 +0200

    insert statement

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -9,4 +9,4 @@ create table issues (
     update_time timestamp NULLABLE
 );
 
-insert into users values (0, 'xXRausAusDenSchulden69Xx', 'Peter', 'Zwegat', current_date, current_timestamp, 'SCHULDENBERATER', current_timestamp)
+insert into issues values (null,'34d93c95-f279-453c-8269-ac16efefded6', 'xXRausAusDenSchulden69Xx', null, null, current_date, 'SCHULDENBERATER', null)
