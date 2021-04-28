74be34805e421888eb2033026c0ef64ae4914d5b
Niels Korschinsky <niels.korschinsky@mni.thm.de>
2021-04-28 15:41:35 +0200

    naming

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,12 +1,12 @@
 create table issues (
     id uuid primary key DEFAULT uuid_generate_v4(),
-    prjID uuid,
+    prjId uuid,
     message varchar(100),
-    userID uuid NULLABLE,
+    userId uuid NULLABLE,
     deadline date NULLABLE,
-    create_time timestamp,
-    global_role varchar(20),
-    update_time timestamp NULLABLE
+    createTime timestamp,
+    globalRole varchar(20),
+    updateTime timestamp NULLABLE
 );
 
 insert into issues values (null,'34d93c95-f279-453c-8269-ac16efefded6', 'xXRausAusDenSchulden69Xx', null, null, current_date, 'SCHULDENBERATER', null)
