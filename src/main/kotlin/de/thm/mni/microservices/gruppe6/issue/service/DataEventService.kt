a7df7895d3ea75e35a7adc0b3bcee2e4095df23f
Dominik Kröll <dominik.kroell@mni.thm.de>
2021-05-18 18:28:05 +0200

    DataEventService processDataEvent

--- src/main/kotlin/de/thm/mni/microservices/gruppe6/issue/service/DataEventService.kt
+++ src/main/kotlin/de/thm/mni/microservices/gruppe6/issue/service/DataEventService.kt
@@ -1,6 +1,5 @@
 package de.thm.mni.microservices.gruppe6.issue.service
 
-import de.thm.mni.microservices.gruppe6.issue.model.persistence.ProjectRepository
 import de.thm.mni.microservices.gruppe6.lib.event.DataEvent
 import de.thm.mni.microservices.gruppe6.lib.event.*
 import org.slf4j.Logger
@@ -8,34 +7,21 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 
 @Service
-class DataEventService(@Autowired val issueDbService: IssueDbService,
-                       @Autowired val projectDbService: ProjectDbService,
+class DataEventService(@Autowired val projectDbService: ProjectDbService,
                        @Autowired val userDbService: UserDbService) {
 
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-
     fun processDataEvent(dataEvent: Mono<DataEvent>) {
-        dataEvent.map {
-            when(it) {
-                is ProjectEvent -> {
-                    projectDbService.receiveUpdate(it)
-                }
-                is UserEvent -> {
-                    userDbService.receiveUpdate(it)
-                }
-                is IssueEvent -> {}
-                else -> error("Unexpected Event type: ${it?.javaClass}")
-            }
-        }
-        TODO("Verarbeitung der Errors bzw des Rückgabewertes")
+        dataEvent.ofType(ProjectEvent::class.java)
+            .doOnNext { logger.debug("ProjectEvent {}", it.id) }
+            .subscribe { projectDbService.receiveUpdate(it) }
 
-        // dataEvent.ofType(ProjectEvent::class.java).publish { projectDbService::receiveUpdate }
-
+        dataEvent.ofType(UserEvent::class.java)
+            .doOnNext { logger.debug("UserEvent {}", it.id) }
+            .subscribe { userDbService.receiveUpdate(it) }
     }
-
 
 }
